b2ef9061d4cf048bb9761a4b121a23be91940fe9
Kaya <kaya_chen@outlook.com>
2025-02-23 20:46:49 +0800

    React Router Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
-import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route, useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+  const navigate = useNavigate();
+  return <button onClick={() => { navigate('/login') }}>登出</button>
+}
 
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+  return (
+    <>
+      <p>這是 Todo 頁面</p>
+      <Logout />
+    </>
+  )
 };
+
 const Login = () => {
   return <p>這是登入頁面</p>;
 };
